3b97a6709fde42ec33f0a15e2fdbe7b5b03aff61
Evgeniy Manachkin <sfstudio@mail.ru>
2015-11-07 06:44:58 +0600

    add netcwnmp - basic tr_069 agent support, need programmer to finish implementation is required by ISPs

---
+++ DoConfigure.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "==================CONFIGURE-NETWCMP=============================="
+APROOTDIR=`pwd`
+
+HBUILD=`uname -m`-pc-linux-gnu
+HTARGET=mipsel-linux
+
+if [ ! -f $APROOTDIR/configure ]; then
+    aclocal
+    autoreconf -fi
+    autoconf
+fi
+if [ ! -f $APROOTDIR/Makefile.in ]; then
+    automake --add-missing --force-missing --copy
+fi
+
+CONFOPTS="--host=$HTARGET --target=$HTARGET --build=$HBUILD"
+CONFOPTS="$CONFOPTS --prefix=$APROOTDIR/filesystem --disable-dependency-tracking"
+CONFOPTS="$CONFOPTS --enable-shared=no --enable-static=no"
+
+./configure $CONFOPTS
