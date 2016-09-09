e340a2e39c190e9f038373e3b761cfb558b2d1bc
Noktoborus <noktoborus@gmail.com>
2016-09-09 17:52:10 +0300

    netcwmpd: add LANHostConfigManagement.DHCPStaticAddress from Baseline:1 skel

--- cwmpd/src/modules/InternetGatewayDevice/InternetGatewayDevice.c
+++ cwmpd/src/modules/InternetGatewayDevice/InternetGatewayDevice.c
@@ -38,7 +38,7 @@ cpe_reload_all(cwmp_t *cwmp, callback_register_func_t callback_reg)
 #include "WANDevice/WANConnectionDevice/WANPPPConnection.c"
 #include "Layer3Forwarding/Layer3Forwarding.c"
 #include "WANDevice/WANEthernetInterfaceConfig/WANEthernetInterfaceConfig.c"
-
+#include "LANDevice/LANHostConfigManagement/IPInterface/IPInterface.c"
 #include "LANDevice/LANDevice.c"
 #include "LANDevice/X_COM_IgmpSnoopingConfig/X_COM_IgmpSnoopingConfig.c"
 #include "LANDevice/LANHostConfigManagement/LANHostConfigManagement.c"
